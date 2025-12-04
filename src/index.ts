dd2547e5a6f2527cca4f4b4c760c9a584b846dd7
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-12-04 12:03:11 +0100

    Refactor as broken file extension

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,78 @@
 import { ILayoutRestorer } from "@jupyterlab/application";
-import { ICommandPalette, MainAreaWidget, WidgetTracker } from "@jupyterlab/apputils";
+import { WidgetTracker } from "@jupyterlab/apputils";
+import { ITranslator } from "@jupyterlab/translation";
 import type { JupyterFrontEnd, JupyterFrontEndPlugin } from "@jupyterlab/application";
+import type { IDocumentWidget } from "@jupyterlab/docregistry";
 
-import { ArrowGridViewer } from "./widget";
+import { ArrowGridViewerFactory } from "./widget";
+import type { ArrowGridViewer } from "./widget";
 
-function activate(
-  app: JupyterFrontEnd,
-  palette: ICommandPalette,
-  restorer: ILayoutRestorer | null,
-) {
-  console.log("JupyterLab extension diana is activated!");
-
-  // Declare a widget variable
-  let widget: MainAreaWidget<ArrowGridViewer>;
-
-  // Add an application command
-  const command: string = "diana:open";
-  app.commands.addCommand(command, {
-    label: "Open dataframe viewer",
-    execute: () => {
-      if (!widget || widget.isDisposed) {
-        const content = new ArrowGridViewer();
-        widget = new MainAreaWidget({ content });
-        widget.addClass("diana-base");
-        widget.id = "diana";
-        widget.title.label = "Dataframe viewer";
-        widget.title.closable = true;
-      }
-      if (!tracker.has(widget)) {
-        // Track the state of the widget for later restoration
-        tracker.add(widget);
-      }
-      if (!widget.isAttached) {
-        app.shell.add(widget, "main");
-      }
-
-      // Activate the widget
-      app.shell.activateById(widget.id);
-    },
-  });
-
-  // Add the command to the palette.
-  palette.addItem({ command, category: "Tutorial" });
-
-  // Track and restore the widget state
-  const tracker = new WidgetTracker<MainAreaWidget<ArrowGridViewer>>({
-    namespace: "diana",
-  });
-  if (restorer) {
-    restorer.restore(tracker, {
-      command,
-      name: () => "diana",
-    });
-  }
-}
-
-/**
- * Initialization data for the jupyterdiana extension.
- */
-const plugin: JupyterFrontEndPlugin<void> = {
-  id: "jupyterdiana:plugin",
-  description: "Arrow viewer for Jupyter",
-  requires: [ICommandPalette],
+const arrowGrid: JupyterFrontEndPlugin<void> = {
+  activate: activateArrowGrid,
+  id: "@jupyterdiana/arrowgridviewer-extension:arrowgrid",
+  description: "Adds viewer for file that can be read into Arrow format.",
+  requires: [ITranslator],
   optional: [ILayoutRestorer],
   autoStart: true,
-  activate: activate,
 };
 
-export default plugin;
+function activateArrowGrid(
+  app: JupyterFrontEnd,
+  translator: ITranslator,
+  restorer: ILayoutRestorer | null,
+): void {
+  const factory_arrow = "ArrowTable";
+
+  app.docRegistry.addFileType({
+    name: "parquet",
+    displayName: "Parquet",
+    mimeTypes: ["application/vnd.apache.parquet"],
+    extensions: [".parquet"],
+    contentType: "file",
+    fileFormat: "base64",
+  });
+
+  const trans = translator.load("jupyterlab");
+
+  const factory = new ArrowGridViewerFactory({
+    name: factory_arrow,
+    label: trans.__("Arrow Dataframe Viewer"),
+    fileTypes: ["parquet", "csv"],
+    defaultFor: ["parquet"],
+    readOnly: true,
+    translator,
+  });
+  const tracker = new WidgetTracker<IDocumentWidget<ArrowGridViewer>>({
+    namespace: "arrowviewer",
+  });
+
+  if (restorer) {
+    void restorer.restore(tracker, {
+      command: "docmanager:open",
+      args: (widget) => ({ path: widget.context.path, factory: factory_arrow }),
+      name: (widget) => widget.context.path,
+    });
+  }
+
+  app.docRegistry.addWidgetFactory(factory);
+  const ft = app.docRegistry.getFileType("parquet");
+
+  factory.widgetCreated.connect(async (_sender, widget) => {
+    // Track the widget.
+    void tracker.add(widget);
+    // Notify the widget tracker if restore data needs to update.
+    widget.context.pathChanged.connect(() => {
+      void tracker.save(widget);
+    });
+
+    if (ft) {
+      widget.title.icon = ft.icon!;
+      widget.title.iconClass = ft.iconClass!;
+      widget.title.iconLabel = ft.iconLabel!;
+    }
+
+    await widget.content.ready;
+  });
+}
+
+export default arrowGrid;
--- src/widget.ts
+++ src/widget.ts
@@ -1,12 +1,22 @@
+import { ABCWidgetFactory, DocumentWidget } from "@jupyterlab/docregistry";
+import { PromiseDelegate } from "@lumino/coreutils";
 import { DataGrid } from "@lumino/datagrid";
 import { Panel } from "@lumino/widgets";
+import type { DocumentRegistry, IDocumentWidget } from "@jupyterlab/docregistry";
 import type * as DataGridModule from "@lumino/datagrid";
 
 import { ArrowModel } from "./model";
 
+export namespace ArrowGridViewer {
+  export interface IOptions {
+    context: DocumentRegistry.Context;
+  }
+}
+
 export class ArrowGridViewer extends Panel {
-  constructor() {
+  constructor(options: ArrowGridViewer.IOptions) {
     super();
+    this._context = options.context;
     this.addClass("arrow-viewer");
 
     this._grid = new DataGrid({
@@ -19,12 +29,57 @@ export class ArrowGridViewer extends Panel {
     });
     this._grid.addClass("arrow-grid-viewer");
     this.addWidget(this._grid);
-    this.updateGrid();
+    this._ready = this.initialize();
   }
 
-  async updateGrid() {
+  get ready(): Promise<void> {
+    return this._ready;
+  }
+
+  get revealed(): Promise<void> {
+    return this._revealed.promise;
+  }
+
+  protected async initialize(): Promise<void> {
+    await this._context.ready;
+    await this._updateGrid();
+    this._revealed.resolve(undefined);
+  }
+
+  private async _updateGrid() {
     this._grid.dataModel = await ArrowModel.fetch("data/gen/test.parquet");
   }
 
+  private _context: DocumentRegistry.Context;
   private _grid: DataGridModule.DataGrid;
+  private _revealed = new PromiseDelegate<void>();
+  private _ready: Promise<void>;
+}
+
+export namespace ArrowGridDocumentWidget {
+  export interface IOptions extends Omit<DocumentWidget.IOptions<ArrowGridViewer>, "content"> {
+    content?: ArrowGridViewer;
+  }
+}
+
+export class ArrowGridDocumentWidget extends DocumentWidget<ArrowGridViewer> {
+  constructor(options: ArrowGridDocumentWidget.IOptions) {
+    let { content, context, reveal, ...other } = options;
+    content = content || ArrowGridDocumentWidget._createContent(context);
+    reveal = Promise.all([reveal, content.revealed]);
+    super({ content, context, reveal, ...other });
+  }
+
+  private static _createContent(
+    context: DocumentRegistry.IContext<DocumentRegistry.IModel>,
+  ): ArrowGridViewer {
+    return new ArrowGridViewer({ context });
+  }
+}
+
+export class ArrowGridViewerFactory extends ABCWidgetFactory<IDocumentWidget<ArrowGridViewer>> {
+  protected createNewWidget(context: DocumentRegistry.Context): IDocumentWidget<ArrowGridViewer> {
+    const translator = this.translator;
+    return new ArrowGridDocumentWidget({ context, translator });
+  }
 }
